d7ca5b29d9b089375c01b36dabd25cea2cb71da2
MenakaRevel <menaka@rainbow.iis.u-tokyo.ac.jp>
2020-04-22 12:05:39 +0900

    bug fix calc_stat.py

--- img/calc_stat.py
+++ img/calc_stat.py
@@ -27,15 +27,28 @@ def slink(src,dst):
     else:
       raise
 #--
+def make_yearlist(syear,smon,sday,N=365):
+    start_dt=datetime.date(syear,smon,sday)
+    fname="year_day.txt"
+    f=open(fname,"w")
+    for day in np.arange(0,N):
+        nw_dt = start_dt + datetime.timedelta(days=day)
+        yyyy,mm,dd = nw_dt.year, nw_dt.month, nw_dt.day
+        yyyymmdd = "%04d%02d%02d\n"%(yyyy,mm,dd)
+        f.write(yyyymmdd)
+    f.close()
+#--
 slink("../gosh/params.py", "params.py")
 import params as pm
 # define values
 N=366 #2004
 exp="E2O_womc"
-outdir=pm.HydroDA()+"/out/"+exp
+outdir=pm.DA_dir()+"/out/"+exp
 # mkdir stat
 mk_dir(outdir+"/assim_out/stat")
+# make list of days
+#make_yearlist(2004,1,1,N=366)
 # compile
 os.system("ifort calc_stat.f90 -o calc_stat -O3 -assume byterecl -heap-arrays  -g -traceback -free -parallel")
 # run calc_stat
-os.system("./calc_stat "+str(N)+" "+str(pm.ens_mem())+" "+out_dir+" "+pm.CaMa_dir())
+os.system("./calc_stat "+str(N)+" "+str(pm.ens_mem())+" "+outdir+" "+pm.CaMa_dir())
