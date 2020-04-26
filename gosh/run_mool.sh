9e2b39bf10c99ec8dc26ea78e726b27523269738
MenakaRevel <menaka@rainbow.iis.u-tokyo.ac.jp>
2020-04-26 21:23:42 +0900

    womc anomaly

--- gosh/run_mool.sh
+++ gosh/run_mool.sh
@@ -24,7 +24,7 @@ cd $HydroDA
 
 # experiment : edit the experiment name in here and params.py experiment()
 # before running run_mool.sh , please edit the nessary experimental settings in params.py
-EXP="E2O_wmc_02_anomalyDA"
+EXP="E2O_womc_anomalyDA"
 #IFACTOR="1.08"
 
 mkdir $HydroDA"/out"
