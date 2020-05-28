cd915336268dfcc260cbe7a5435bc6522d58f3cc
MenakaRevel <menaka@rainbow.iis.u-tokyo.ac.jp>
2020-05-28 22:15:20 +0900

    bugfix

--- img/sfcgraph_ens.py
+++ img/sfcgraph_ens.py
@@ -24,7 +24,7 @@ import cal_stat as stat
 
 #argvs = sys.argv
 
-experiment="E2O_womc_anomalyDA4_adif"
+experiment="E2O_womc_anomalyDA5_adif"
 #assim_out=pm.DA_dir()+"/out/"+pm.experiment()+"/assim_out"
 assim_out=pm.DA_dir()+"/out/"+experiment+"/assim_out"
 print assim_out
